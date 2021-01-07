aa1ca4e6893b88546e2881c8a5532538c369374f
Tomas Bonanzea <tomasbonanzea98@gmail.com>
2021-01-07 10:08:14 -0300

    segunda entrega terminada

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,47 +1,33 @@
 import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 const NavBar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<a className="navbar-brand" href="#">
-				Navbar
-			</a>
-			<button
-				className="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarNav"
-				aria-controls="navbarNav"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navbarNav">
-				<ul className="navbar-nav">
-					<li className="nav-item active">
-						<a className="nav-link" href="#">
-							Home <span className="sr-only">(current)</span>
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">
-							Features
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link" href="#">
-							Pricing
-						</a>
-					</li>
-					<li className="nav-item">
-						<a className="nav-link disabled" href="#">
-							Disabled
-						</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+		<Navbar className="navbar navbar-dark bg-dark" expand="lg">
+			<Navbar.Brand href="#">ProyectoFinal</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link href="#">Home</Nav.Link>
+					<NavDropdown title="Productos" id="basic-nav-dropdown">
+						<NavDropdown.Item href="#">
+							Categoría 1
+						</NavDropdown.Item>
+						<NavDropdown.Item href="#">
+							Categoría 2
+						</NavDropdown.Item>
+						<NavDropdown.Item href="#">
+							Categoría 3
+						</NavDropdown.Item>
+						<NavDropdown.Divider />
+						<NavDropdown.Item href="#">
+							Todos los productos
+						</NavDropdown.Item>
+					</NavDropdown>
+					<Nav.Link href="#">Contacto</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 };
 
