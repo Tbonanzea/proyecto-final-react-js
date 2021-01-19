6ac9febb940e1b05d2c85512a5de052e43197c2d
Tomas Bonanzea <tomasbonanzea98@gmail.com>
2021-01-18 22:12:42 -0300

    4ta entrega

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -4,6 +4,8 @@ import "./ItemCount.css";
 const ItemCount = ({ stock, initial }) => {
 	const [count, setCount] = useState(initial);
 
+	const [agregados, setAgregados] = useState(false);
+
 	const addCount = () => {
 		count < stock && setCount(count + 1);
 	};
@@ -12,24 +14,41 @@ const ItemCount = ({ stock, initial }) => {
 		count > 0 && setCount(count - 1);
 	};
 
-	const onAdd = () => {};
+	const onAdd = () => {
+		count > 0 && setAgregados(true);
+	};
 
 	return (
-		<div id="wrapper" className="col-3 text-center">
-			<div className="row">
-				<button className="col" onClick={subCount}>
+		<div id="wrapper" className="col-3 text-center p-4">
+			<h3 className="mt-2">Nombre del item</h3>
+			<div className="row justify-content-around mt-2">
+				<button
+					className="col-3 rounded-circle btn-outline-primary"
+					onClick={subCount}
+				>
 					-
 				</button>
 
-				<h1 className="col">{count}</h1>
+				<h1 className="col-5">{count}</h1>
 
-				<button className="col" onClick={addCount}>
+				<button
+					className="col-3 rounded-circle btn-outline-primary"
+					onClick={addCount}
+				>
 					+
 				</button>
 			</div>
-			<button className="btn-primary rounded-pill" onClick={onAdd}>
+			<button
+				className="btn-success rounded-pill mb-2 mt-2"
+				onClick={onAdd}
+			>
 				Agregar al Carrito
 			</button>
+			{agregados ? (
+				<p className="mt-2">{count} items agregados al carrito</p>
+			) : (
+				<p className="mt-2">No has agregado items al carrito</p>
+			)}
 		</div>
 	);
 };
